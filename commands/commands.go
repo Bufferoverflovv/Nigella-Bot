42ce46116c86af2339cc68ea0f4354485edb23a1
Jordan <49358147+Bufferoverflovv@users.noreply.github.com>
2022-06-18 20:34:45 +0930

    initial commands structure

--- commands/commands.go
+++ commands/commands.go
@@ -1,1 +1,19 @@
 package commands
+
+import (
+	//"errors"
+	//"fmt"
+	//"log"
+	//"os"
+	//"os/signal"
+	//"strings"
+	//"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	integerOptionMinValue          = 1.0
+	dmPermission                   = false
+	defaultMemberPermissions int64 = discordgo.PermissionManageServer
+)
