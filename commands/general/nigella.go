1c37a81cff9b37d2ade1aa5fce0b9e7c908a8949
Heffy22 <74898262+Heffy22@users.noreply.github.com>
2022-06-19 21:54:13 +0930

    Testing Xmas option

--- commands/general/nigella.go
+++ commands/general/nigella.go
@@ -10,20 +10,32 @@ var
 	DirtyTalkCMD = discordgo.ApplicationCommand{
 		Name:        "nigella",
 		Description: "Dirty Talk",
+        Options: []*discordgo.ApplicationCommandOption{
+            {
+            Type:        discordgo.ApplicationCommandOptionString,
+            Name:        "xmas",
+            Description: "XXXmas",
+            Required:    false,
 	}
 
 	DirtyTalkCommand = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
         {
 		"nigella": func(s *discordgo.Session, i *discordgo.InteractionCreate)
             {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse
+			options := i.ApplicationCommandData().Options
                 {
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData
-                    {
+				if option, ok := optionMap["xmas"]; ok {
+				margs = append(margs, option.StringValue())
+				msgformat += "https://www.youtube.com/watch?v=2wncEeJZqzM"{
+                    }
+                }
+                else s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+                    Type: discordgo.InteractionResponseChannelMessageWithSource,
+                    Data: &discordgo.InteractionResponseData{
 					Content: "https://www.youtube.com/watch?v=RtS2Ikk7A9I",
-                    },
-                })
-            },
+                    }
+                    })
+                }
+            }
         }
     )
